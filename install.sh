4fecc121fb2c5b017c68f3a3185913d183174692
Pau Costa Ferrer <mico@MicoMain.localdomain>
2022-01-03 00:17:58 +0100

    First commit.

---
+++ install.sh
@@ -0,0 +1,36 @@
+# install nix
+curl -L https://nixos.org/nix/install | sh
+
+# source nix
+. ~/.nix-profile/etc/profile.d/nix.sh
+
+# install packages
+nix-env -iA \
+	nixpkgs.git \
+	nixpkgs.zsh \
+	nixpkgs.antibody \
+	nixpkgs.neovim \
+	nixpkgs.tmux \
+	nixpkgs.stow \
+	nixpkgs.yarn \
+	nixpkgs.fzf \
+	nixpkgs.ripgrep \
+	nixpkgs.bat \
+	nixpkgs.direnv \
+    nixpkgs.gh
+# stow
+stow git
+stow zsh
+stow nvim
+
+# enable zsh in /etc/shells 
+command -v zsh | sudo tee -a /etc/shells
+
+# make zsh default shell
+sudo chsh -s $(which zsh) $USER
+
+# bundle zsh plugins with antibody
+antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+
+
+
